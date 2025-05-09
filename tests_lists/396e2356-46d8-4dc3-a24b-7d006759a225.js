54261d75110a9dda728049f1e7859821970a9808
Lior Goldemberg <lior.goldemberg@global-e.com>
2025-05-09 15:54:58 +0300

    merge words

--- tests_lists/396e2356-46d8-4dc3-a24b-7d006759a225.js
+++ tests_lists/396e2356-46d8-4dc3-a24b-7d006759a225.js
@@ -1,28 +1,8 @@
 tests_list = [
-    {
-
-        scriptUrl: "/2025-04-en-1.js",
-        name: "אנגלית 4/25 - 1",
-        lang: 'en',
-    },
-    {
-        scriptUrl: "/2025-04-en-2.js",
-        name: "אנגלית 4/25 - 2",
-        lang: 'en',
-    },
-    {
-        scriptUrl: "/2025-04-en-3.js",
-        name: "אנגלית 4/25 - 3",
-        lang: 'en',
-    },
-    {
-        scriptUrl: "/2025-04-en-4.js",
-        name: "אנגלית 4/25 - 4",
-        lang: 'en',
-    },
+    
     {
         scriptUrl: "/2025-04-en-5.js",
-        name: "אנגלית 4/25 - 5",
+        name: "אנגלית 5/25",
         lang: 'en',
     },
  
--- tests_lists/64cdd390-6bb7-4a8b-b0e0-b52294368613.js
+++ tests_lists/64cdd390-6bb7-4a8b-b0e0-b52294368613.js
@@ -1,47 +1,14 @@
 tests_list = [
-    {
-        scriptUrl: "2025-may-en-1.js",
-        name: "אנגלית 5/25 - 1",
-        lang: 'en',
-    },
-    {
-        scriptUrl: "2025-may-en-2.js",
-        name: "אנגלית 5/25 - 2",
-        lang: 'en',
-    },
-    {
-        scriptUrl: "2025-may-en-3.js",
-        name: "אנגלית 5/25 - 3",
-        lang: 'en',
-    },
-    {
-        scriptUrl: "2025-may-en-4.js",
-        name: "אנגלית 5/25 - 4",
-        lang: 'en',
-    },
+  
     {
         scriptUrl: "2025-may-en-5.js",
-        name: "אנגלית 5/25 - 5",
+        name: "אנגלית 5/25",
         lang: 'en',
     },
-    {
-        scriptUrl: "2025-mar-fr-1.js",
-        name: "צרפתית 3/25 - 1",
-        lang: 'fr',
-    },
-    {
-        scriptUrl: "2025-mar-fr-2.js",
-        name: "צרפתית 3/25 - 2",
-        lang: 'fr',
-    },
-    {
-        scriptUrl: "2025-mar-fr-3.js",
-        name: "צרפתית 3/25 - 3",
-        lang: 'fr',
-    },
+  
     {
         scriptUrl: "2025-mar-fr-4.js",
-        name: "צרפתית 3/25 - 4",
+        name: "צרפתית 3/25",
         lang: 'fr',
     },
     {
@@ -49,41 +16,19 @@ tests_list = [
         name: "אנגלית 12/24",
         lang: 'en',
     },
-    {
-
-        scriptUrl: "2024-nov-fr-1.js",
-        name: "צרפתית 11/24 - 1",
-        lang: 'fr',
-    },
-    {
-
-        scriptUrl: "2024-nov-fr-2.js",
-        name: "צרפתית 11/24 - 2",
-        lang: 'fr',
-    },
+    
     {
 
         scriptUrl: "2024-nov-fr-3.js",
-        name: "צרפתית 11/24 - 3",
+        name: "צרפתית 11/24",
         lang: 'fr',
     },
 
-    {
-
-        scriptUrl: "2024-nov-en-1.js",
-        name: "אנגלית 11/24 - 1",
-        lang: 'en',
-    },
-    {
-
-        scriptUrl: "2024-nov-en-2.js",
-        name: "אנגלית 11/24 - 2",
-        lang: 'en',
-    },
+    
     {
 
         scriptUrl: "2024-nov-en-3.js",
-        name: "אנגלית 11/24 - 3",
+        name: "אנגלית 11/24",
         lang: 'en',
     },
     {
